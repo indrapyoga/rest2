3c60a11c8a55baf2e3ba8453cde680ec05395497
DESKTOP-37Q3L3S\Indra Prayoga S <indraprayogas21@gmail.com>
2022-02-17 22:17:42 +0700

    first commit

---
+++ rest2.py
@@ -0,0 +1,27 @@
+from flask import Flask,request,jsonify
+from flask_restful import Resource,Api
+from flask_cors import CORS
+
+app = Flask(__name__)
+api = Api(app)
+CORS(app)
+
+identitas = {}
+
+class KResource(Resource):
+    def get(self):
+        response = identitas
+        return response
+    
+    def post(self):
+        nama = request.form["nama"]
+        nim = request.form["nim"]
+        identitas["nama mahasiswa"] = nama
+        identitas["nim mahassiwa"] = nim
+        return jsonify({"msg":"berhasil ditambah"})
+
+api.add_resource(KResource,"/api",methods=["GET","POST"])
+
+if __name__ == '__main__':
+    app.run(debug=True,port=5011)
+    
